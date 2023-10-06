1f41b96663c2b24954828f4444c22e160d8c7a20
SRK <RanjithSrikonda.Devops@example.com>
2023-10-06 11:08:01 +0530

    Jenkins

---
+++ 03-mongodb/mongodb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/centos
+git clone https://github.com/githubSRK1/roboshop-shell.git
+cd roboshop-shell
+sh mongodb.sh
---
+++ README.md
@@ -0,0 +1,2 @@
+# roboshop-infra-standard
+# roboshop-infra-standard
