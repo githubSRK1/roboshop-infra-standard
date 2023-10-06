247306ee1375b5dd05d7d7ae73f09ba9c355baef
SRK <RanjithSrikonda.Devops@example.com>
2023-10-06 16:20:24 +0530

    qw

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,11 +1,12 @@
 module "vpc" {
-  source = "../../terraform-aws-vpc-advanced"
+  source = "../terraform-aws-vpc-advanced"
   project_name = var.project_name #"roboshop" instead of hard coding we are keeping in variables.tf
   cidr_block = var.cidr_block
   common_tags = var.common_tags
   public_subnet_cidr = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
   database_subnet_cidr = var.database_subnet_cidr
+
 
     #peering
   is_peering_required = true
